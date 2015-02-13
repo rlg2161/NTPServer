bb14c7527fb335289e6a0bb2b30529b65b1048b5
Ross Gruber <ross.gruber@gmail.com>
2015-02-13 12:17:37 -0800

    file re-org

--- NTPserver.js
+++ NTPserver.js
@@ -1,85 +1,100 @@
-
-var util = require('util')
-var EventEmitter = require('events').EventEmitter;
-
-var producer = function(consumer){
-
-  var self = this;
-
-  setTimeout(function(){
-    self.emit('open', consumer);
-  }, 0);
-
-  setTimeout(function(){
-    self.emit('close', consumer)
-  }, 10000);
+var EE = require('events').EventEmitter;
+var util = require('util');
 
 
-  setInterval(function(){
-    self.emit('time', consumer);
-  }, 1000)
+function producer() {
 
-  this.on('newListener', function(listener){
-    console.log('Event Listener: ' + listener);
-  });
-}
+  this.consumerList = [];
 
-util.inherits(producer, EventEmitter);
+  this.pEmitter = new producerEmitter();
+  this.pListener = new producerListener(this.consumerList);
 
-module.exports = producer;
-
-
-
-/*var consumer = new EventEmitter();  // ---> Logs the time every second
-
-consumer.on('sendTime', function(){
-  date = new Date();
-  console.log(date);
-})
-
-setInterval(function (){
-  consumer.emit('sendTime');
-}, 1000);
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*function Producer(numConsumers){
-
-  this.producers = 
-}
-
-
-
-function Consumer(){
-
-}
-
-Consumer.prototype.register = function(){
-
-}
-
-Consumer.prototype.keepAlive = function(){
-
-}
-
-Consumer.prototype.logMessage = function(message){
   
-  var thread = 
-  console.log(message)
+}
 
-}*/
+
+function consumer() {
+
+  this.cEmitter = new consumerEmitter();
+  this.cListener = new consumerListener(producer);
+}
+
+
+var producerEmitter = function(){
+  EE.call(this);
+
+  this.time = function(){
+    var date = new Date();
+    this.emit('time', date);
+  }
+
+};
+
+var producerListener = function(consumerList){
+
+  this.registerHandler = function(consumerList){
+    consumerList.append(10);
+  }
+
+  this.keepAliveHandler = function(consumerList){
+    consumerList[0] = 10;
+  }
+
+}
+
+var consumerEmitter = function(){
+
+  this.register = function(){
+    this.emit('register');
+  }
+
+  this.keepAlive = function(){
+    this.emit('keepAlive');
+  }
+}
+
+
+
+var consumerListener = function(){
+
+  this.timeHandler = function(date){
+    console.log(date);
+  }
+}
+
+util.inherits(producerEmitter, EE);
+util.inherits(consumerEmitter, EE);
+
+var iID = null;
+var tID = null;
+
+function beginProcess(){
+  producer.pEmitter.time();
+  iID = setInterval(function(){
+    producer.pEmitter.time();
+  }, 1000);
+
+  tID = setTimeout(function(){
+    clearInterval(iID);
+  }, 10000);
+}
+
+function resetProcess(){
+  clearInterval(iID);
+  clearTimeout(tID);
+  beginProcess();
+}
+
+var producer = new producer();
+var consumer = new consumer(producer)
+
+producer.pEmitter.on('time', consumer.cListener.timeHandler);
+//consumer.on('register', producer.)
+
+beginProcess();
+setTimeout(function(){
+  console.log("timer reset");
+  resetProcess();
+}, 5000);
+
+
