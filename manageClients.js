cf1edd365b25585c08681514dd46da7bf252e766
Ross Gruber <ross.gruber@gmail.com>
2015-02-14 22:48:06 -0800

    Works for one client

--- manageClients.js
+++ manageClients.js
@@ -1,9 +1,7 @@
 var createClient = require('./udpClient.js');
-//var createServer = require('./udpServer.js');
 
-//createServer();
+createClient(10001);
 
-var numClients = process.argv[2];
-for (var i = 0; i < numClients; i++){
-  createClient(10001+i);
-}
+
+
+
